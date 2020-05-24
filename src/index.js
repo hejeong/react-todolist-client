b52be9c49bda35639ebbc703b1fe06d63599e79f
hejeong <jonhong777@gmail.com>
2020-05-24 15:53:42 -0400

    Add redux tools to application

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import App from './App.js';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom'; 
 
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const reducers = combineReducers({})
+const store = createStore(reducers, composeEnhancer(applyMiddleware(thunk)));
+
+
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>,
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>,
     document.getElementById('root')
 );
