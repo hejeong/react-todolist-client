1b4dcaa4ac2f7487643ff4995f7ad443bef2fbec
hejeong <jonhong777@gmail.com>
2020-06-10 14:01:40 -0400

    Upload

--- src/Home.js
+++ src/Home.js
@@ -7,24 +7,17 @@ import { Button, Space, Typography } from 'antd';
 const { Title } = Typography;
 
 function Home({loggedIn}) {
-  if(!loggedIn){
-    return (
-      <div className="home">
-        <Title>Welcome to WhatTodo</Title>
-        <Space align='center' size={30} className='login-and-signup' >
-          <Button type='primary' className='login-btn'><Link to='/login'>Login</Link></Button>
-          <Button type='primary'><Link to='/signup'>Signup</Link></Button>
-        </Space>
-      </div>
-    )
-  }else {
-    return (
-      <div className="home">
-        <Title>Welcome to WhatTodo</Title>
-        <Button type='primary'><Link to='/todos'>View my Todolist</Link></Button>
-      </div>
-    )
-  }
+  return loggedIn ? <div className="home">
+                      <Title>Welcome to WhatTodo</Title>
+                      <Button type='primary'><Link to='/todos'>View my Todolist</Link></Button>
+                    </div>
+                  : <div className="home">
+                      <Title>Welcome to WhatTodo</Title>
+                      <Space align='center' size={30} className='login-and-signup' >
+                        <Button type='primary' className='login-btn'><Link to='/login'>Login</Link></Button>
+                        <Button type='primary'><Link to='/signup'>Signup</Link></Button>
+                      </Space>
+                    </div> 
 }
 
 const mapStateToProps = (state) => {
